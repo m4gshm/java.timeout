ae815159a5e82bed48f13d2701ffe82a4eb9cd44
Bulgakov Alexander <asbulgakov@alfabank.ru>
2019-08-01 16:22:28 +0300

    disable default deadline

--- src/main/java/timeout/servlet/ExpiresHeaderFilter.java
+++ src/main/java/timeout/servlet/ExpiresHeaderFilter.java
@@ -12,13 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 import java.time.Duration;
 import java.util.function.Function;
 
-import static java.time.Duration.ofMinutes;
 import static java.util.Objects.requireNonNull;
 import static timeout.DeadlineHolder.calc;
 import static timeout.http.HttpHeaders.EXPIRES_HEADER;
 
 @Slf4j
-
 public class ExpiresHeaderFilter implements Filter {
 
     private final String headerName;
@@ -27,17 +25,18 @@ public class ExpiresHeaderFilter implements Filter {
     private final DeadlineExecutor executor;
 
     public ExpiresHeaderFilter(DeadlineExecutor executor) {
-        this(ofMinutes(1), executor);
+        this(null, executor);
     }
 
     public ExpiresHeaderFilter(Duration defaultDeadline, DeadlineExecutor executor) {
         this(EXPIRES_HEADER, HttpDateHelper::parseHttpDate, defaultDeadline, executor);
     }
 
-    public ExpiresHeaderFilter(String headerName, Function<String, Long> parser, Duration defaultDeadline, DeadlineExecutor executor) {
+    public ExpiresHeaderFilter(String headerName, Function<String, Long> parser,
+                               Duration defaultDeadline, DeadlineExecutor executor) {
         this.headerName = requireNonNull(headerName, "headerName");
         this.parser = requireNonNull(parser, "parser");
-        this.defaultDeadline = requireNonNull(defaultDeadline, "defaultDeadline");
+        this.defaultDeadline = defaultDeadline;
         this.executor = requireNonNull(executor, "executor");
     }
 
@@ -51,7 +50,7 @@ public class ExpiresHeaderFilter implements Filter {
         val httpServletRequest = (HttpServletRequest) request;
         val expires = httpServletRequest.getHeader(headerName);
         var deadline = parser.apply(expires);
-        if (deadline == null) deadline = calc(defaultDeadline, log);
+        if (deadline == null && defaultDeadline != null) deadline = calc(defaultDeadline, log);
         executor.run(deadline, () -> goChain(chain, request, response));
     }
 
