6f5716d37993c19e1386ccf7d62ebcf1c14fd673
Bulgakov Alexander <buls@yandex.ru>
2021-02-24 09:48:15 +0300

    adds modularity (draft)

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,37 @@ if (!asSubproject) configure<io.spring.gradle.dependencymanagement.internal.dsl.
     }
 }
 
+dependencies {
+    listOf(
+        "org.projectlombok:lombok"
+    ).forEach { processor ->
+        annotationProcessor(processor)
+        compileOnly(processor)
+        testAnnotationProcessor(processor)
+        testCompileOnly(processor)
+    }
+
+    compileOnly("javax.servlet:javax.servlet-api")
+    compileOnly("io.github.openfeign:feign-core")
+    compileOnly("org.springframework.cloud:spring-cloud-openfeign-core")
+    implementation("org.slf4j:slf4j-api")
+    implementation("io.projectreactor:reactor-core")
+
+    testImplementation("junit:junit")
+
+    if (JavaVersion.current().isJava9Compatible) {
+        compileOnly("javax.xml.ws:jaxws-api:[2,)")
+    }
+}
+
+tasks.compileJava {
+    options.apply {
+        compilerArgs.add("-Xlint:unchecked")
+        isDeprecation = true
+        isWarnings = false
+    }
+}
+
 if (JavaVersion.current().isJava9Compatible) {
     java {
         sourceCompatibility = JavaVersion.VERSION_1_9
@@ -56,25 +87,3 @@ if (JavaVersion.current().isJava9Compatible) {
     }
 }
 
-dependencies {
-    listOf(
-            "org.projectlombok:lombok"
-    ).forEach { processor ->
-        annotationProcessor(processor)
-        compileOnly(processor)
-        testAnnotationProcessor(processor)
-        testCompileOnly(processor)
-    }
-
-    compileOnly("javax.servlet:javax.servlet-api")
-    compileOnly("io.github.openfeign:feign-core")
-    compileOnly("org.springframework.cloud:spring-cloud-openfeign-core")
-    implementation("org.slf4j:slf4j-api")
-    implementation("io.projectreactor:reactor-core")
-
-    testImplementation("junit:junit")
-
-    if (JavaVersion.current().isJava9Compatible) {
-        compileOnly("javax.xml.ws:jaxws-api:[2,)")
-    }
-}
