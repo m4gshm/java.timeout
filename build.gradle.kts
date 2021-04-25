8ccb811772166aa68d6ee0740c1ee51de4596d65
Bulgakov Alexander <mfourgeneralsherman@gmail.com>
2021-04-26 00:23:29 +0300

    gradle build refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,21 @@
-buildscript {
-    var dependencyManagementPluginVer: String? by rootProject.extra
-    if (dependencyManagementPluginVer == null) {
-        dependencyManagementPluginVer = "1.0.11.RELEASE"
-    }
+group = "m4gshm.java.timeout"
+version = "0.0.1-SNAPSHOT"
 
-    val useMavenCentral: Boolean? by project.extra
-    if (useMavenCentral != false) {
-        repositories {
-            mavenCentral()
-        }
-    }
-    dependencies {
-        classpath("io.spring.gradle:dependency-management-plugin:$dependencyManagementPluginVer")
-    }
+repositories {
+    mavenCentral()
 }
-
-group = "m4gshm.timeout"
-version = "0.0.1.SNAPSHOT"
-
-repositories.addAll(rootProject.buildscript.repositories)
 
 plugins {
     `java-library`
     `maven-publish`
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
-val asSubproject = pluginManager.hasPlugin("io.spring.dependency-management")
-if (!asSubproject) apply<io.spring.gradle.dependencymanagement.DependencyManagementPlugin>()
-
-if (!asSubproject) configure<io.spring.gradle.dependencymanagement.internal.dsl.StandardDependencyManagementExtension> {
+dependencyManagement {
     dependencies {
         dependency("org.projectlombok:lombok:[1.18,)")
         dependency("org.slf4j:slf4j-api:[1.7,)")
-        dependency("javax.servlet:javax.servlet-api:3.0.1")
+        dependency("javax.servlet:javax.servlet-api:[3.0,)")
         dependency("io.github.openfeign:feign-core:[10,)")
         dependency("io.projectreactor:reactor-core:[0.9,)")
         dependency("org.springframework.cloud:spring-cloud-openfeign-core:[2,)") {
@@ -55,9 +38,11 @@ dependencies {
     compileOnly("javax.servlet:javax.servlet-api")
     compileOnly("io.github.openfeign:feign-core")
     compileOnly("org.springframework.cloud:spring-cloud-openfeign-core")
-    implementation("org.slf4j:slf4j-api")
-    implementation("io.projectreactor:reactor-core")
+    compileOnly("org.slf4j:slf4j-api")
+    compileOnly("io.projectreactor:reactor-core")
 
+    testImplementation("org.slf4j:slf4j-api")
+    testImplementation("io.projectreactor:reactor-core")
     testImplementation("junit:junit")
 
     val javaVersion = JavaVersion.current()
@@ -68,7 +53,7 @@ dependencies {
     }
 }
 
-tasks.withType<JavaCompile> {
+tasks.compileJava {
     options.apply {
         compilerArgs.add("-Xlint:unchecked")
         isDeprecation = true
@@ -77,54 +62,89 @@ tasks.withType<JavaCompile> {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
+    withSourcesJar()
 }
 
-var buildAsJava9Module: Boolean? by project.extra
-val javaVersion = JavaVersion.current()
-if (buildAsJava9Module != false && javaVersion.isJava9Compatible) {
-    project.logger.warn("build as java 9 module")
-    val version9 = "9"
-    val sourceSetJava9 = sourceSets.create("java$version9") {
-        compileClasspath = sourceSets["main"].compileClasspath
-        java {
-            srcDirs("src/main/module")
-            include("module-info.java")
-        }
+val buildForJava8 by extra { true }
+if (!buildForJava8) {
+    java {
+        targetCompatibility = JavaVersion.VERSION_1_9
+        sourceCompatibility = JavaVersion.VERSION_1_9
+        modularity.inferModulePath.set(true)
     }
-
-    val java8Classes = tasks.getByName(sourceSetJava9.compileJavaTaskName, JavaCompile::class) {
-        sourceCompatibility = version9
-        targetCompatibility = version9
-//        if (!javaVersion.isJava9) {
-//            options.compilerArgs.addAll(listOf("--release", version9))
-//        }
-    }
-
-    tasks.jar {
-        dependsOn(java8Classes)
-        into("META-INF/versions/$version9") {
-            from(sourceSetJava9.output)
-        }
-
-        manifest {
-            attributes(mapOf("Multi-Release" to true))
-        }
-    }
+    sourceSets["main"].java.srcDirs("src/main/module")
 
     apply {
         from("./automaticModules.gradle.kts")
     }
-
-    java {
-        modularity.inferModulePath.set(true)
-    }
-
 } else {
-    project.logger.warn("build as standalone module")
+    java {
+        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+    }
     tasks.jar {
         manifest {
-            attributes("Automatic-Module-Name" to "java.timeout")
+            attributes["Automatic-Module-Name"] = "m4gshm.java.timeout"
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("java") {
+            repositories {
+                val mavenTempLocalRepoUrl: String? by rootProject
+                if (mavenTempLocalRepoUrl != null) maven {
+                    name = "mavenTempLocal"
+                    url = uri(mavenTempLocalRepoUrl!!)
+                }
+
+                val mavenRepoSnapshotUrl: String? by rootProject
+                val mavenRepoReleaseUrl: String? by rootProject
+                when {
+                    version.endsWith("-SNAPSHOT") -> mavenRepoSnapshotUrl
+                    else -> mavenRepoReleaseUrl
+                }?.let { repoUrl ->
+                    maven {
+                        name = "mavenCustom"
+                        url = uri(repoUrl)
+                        credentials {
+                            val mavenRepoUser: String? by rootProject
+                            val mavenRepoPassword: String? by rootProject
+                            username = mavenRepoUser
+                            password = mavenRepoPassword
+                        }
+                    }
+                }
+            }
+
+            pom {
+                properties.put("maven.compiler.target", "${java.targetCompatibility}")
+                properties.put("maven.compiler.source", "${java.sourceCompatibility}")
+                developers {
+                    developer {
+                        id.set("m4gshm")
+                        name.set("Bulgakov Alexander")
+                        email.set("buls@yandex.ru")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/m4gshm/java.timeout.git")
+                    developerConnection.set("scm:git:https://github.com/m4gshm/java.timeout.git")
+                    url.set("https://github.com/m4gshm/java.timeout")
+                }
+            }
+
+//            versionMapping {
+//                usage("java-api") {
+//                    fromResolutionOf("runtimeClasspath")
+//                }
+//                usage("java-runtime") {
+//                    fromResolutionResult()
+//                }
+//            }
+
+            from(components["java"])
         }
     }
 }
